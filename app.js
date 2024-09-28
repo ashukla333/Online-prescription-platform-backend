652fa405c53ed31e295eb732836e2c390ee877ec
ashukla333 <amanshukla3747@gmail.com>
2024-09-28 13:47:24 +0530

    ui changes done

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 app.use("/v1", IndexRouter);
 
 app.get("/", (req, res) => {
@@ -27,3 +28,4 @@ app.get("/", (req, res) => {
     console.log(error);
   }
 });
+

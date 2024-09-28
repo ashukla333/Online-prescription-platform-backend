81014a03ea704c1708e8494c4bba1e084c23a199
ashukla333 <amanshukla3747@gmail.com>
2024-09-28 19:59:32 +0530

    age added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ configDotenv({
 
 app.use(
   cors({
-    origin: ["https://blog-mern-nu-one.vercel.app", "http://localhost:3000"],
+    origin: ["https://online-prescription-platform-neon.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
